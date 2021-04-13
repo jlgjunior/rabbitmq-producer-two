b2767a0dce6e3286964d7b4eca567627472c34dd
jlgjunior <jgaribaldijunior@gmail.com>
2021-04-13 09:52:33 -0300

    Add default conversion method to send messages

---
+++ src/main/java/com/course/rabbitmqtwo/producer/DummyProducer.java
@@ -0,0 +1,18 @@
+package com.course.rabbitmqtwo.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.rabbitmqtwo.entity.DummyMessage;
+
+@Service
+public class DummyProducer {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	public void sendDummy(DummyMessage message) {
+		rabbitTemplate.convertAndSend("x.dummy", "", message);
+	}
+}
